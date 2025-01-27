99b9496dce6716ad483b2ad7f08eaa0c544904cf
Hassan Zafar <114862283+hassanzfr@users.noreply.github.com>
2025-01-27 09:52:02 -0500

    Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,18 @@
+namespace flextrainer
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Form2 report = new Form2();
+            Application.Run(report);
+        }
+    }
+}
